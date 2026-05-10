6196c29d13abae4d1173a488dd35dcfc9aeb9e94
Deep-sea-lab <Deep-sea-lab@users.noreply.github.com>
2026-05-10 04:55:40 +0000

    deploy: cf6ea621cd25a12a1b0d1c160c79118274ed934f

--- js/addon-default-entry.js
+++ js/addon-default-entry.js
@@ -75156,7 +75156,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "useAgents", function() { return useAgents; });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var hooks_useStorageInfo__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! hooks/useStorageInfo */ "./src/addons/addons/02agent/shims/hooks/useStorageInfo.ts");
+/* harmony import */ var _shims_hooks_useStorageInfo__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../shims/hooks/useStorageInfo */ "./src/addons/addons/02agent/shims/hooks/useStorageInfo.ts");
 function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
 function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
 function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
@@ -75183,11 +75183,11 @@ const DEFAULT_AGENTS = [{
   }]
 }];
 function useAgents() {
-  const _useStorageInfo = Object(hooks_useStorageInfo__WEBPACK_IMPORTED_MODULE_1__["default"])("AI_ASSISTANT_AGENTS", DEFAULT_AGENTS),
+  const _useStorageInfo = Object(_shims_hooks_useStorageInfo__WEBPACK_IMPORTED_MODULE_1__["default"])("AI_ASSISTANT_AGENTS", DEFAULT_AGENTS),
     _useStorageInfo2 = _slicedToArray(_useStorageInfo, 2),
     agents = _useStorageInfo2[0],
     setAgents = _useStorageInfo2[1];
-  const _useStorageInfo3 = Object(hooks_useStorageInfo__WEBPACK_IMPORTED_MODULE_1__["default"])("AI_ASSISTANT_CURRENT_AGENT_ID", "default-1-model-1"),
+  const _useStorageInfo3 = Object(_shims_hooks_useStorageInfo__WEBPACK_IMPORTED_MODULE_1__["default"])("AI_ASSISTANT_CURRENT_AGENT_ID", "default-1-model-1"),
     _useStorageInfo4 = _slicedToArray(_useStorageInfo3, 2),
     currentModelId = _useStorageInfo4[0],
     setCurrentModelId = _useStorageInfo4[1];
@@ -75358,7 +75358,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "useAttachmentInteraction", function() { return useAttachmentInteraction; });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var utils_block_helper__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! utils/block-helper */ "./src/addons/addons/02agent/shims/utils/block-helper.ts");
+/* harmony import */ var _shims_utils_block_helper__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../shims/utils/block-helper */ "./src/addons/addons/02agent/shims/utils/block-helper.ts");
 function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
@@ -75397,7 +75397,7 @@ const useAttachmentInteraction = (vm, workspace) => {
           const block = workspace.blockDB_[previewBlockIdRef.current];
           if (block) {
             previewBlockIdRef.current = null;
-            Object(utils_block_helper__WEBPACK_IMPORTED_MODULE_1__["scrollBlockIntoView"])(block, workspace);
+            Object(_shims_utils_block_helper__WEBPACK_IMPORTED_MODULE_1__["scrollBlockIntoView"])(block, workspace);
           } else {
             requestAnimationFrame(handler);
           }
@@ -75434,7 +75434,7 @@ const useAttachmentInteraction = (vm, workspace) => {
     if (workspace && vm.editingTarget && targetId === vm.editingTarget.id) {
       const block = workspace.blockDB_[blockId];
       if (block) {
-        Object(utils_block_helper__WEBPACK_IMPORTED_MODULE_1__["scrollBlockIntoView"])(block, workspace);
+        Object(_shims_utils_block_helper__WEBPACK_IMPORTED_MODULE_1__["scrollBlockIntoView"])(block, workspace);
         return;
       }
     }
